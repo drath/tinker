cb6413d00535cd85be9d31043743de07c17fb250
Devendra <devendra_rath@yahoo.com>
2014-09-11 06:08:58 +0530

    Add smooth scroll effect to navigation and other places

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,2 +1,28 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
+function smoothScroll(e, hash){
+ // prevent default anchor click behavior
+ e.preventDefault();
+
+ // animate
+ $('html, body').animate({
+   scrollTop: $(hash).offset().top
+ 	}, 300, function(){
+
+   // when done, add hash to url
+   // (default click behaviour)
+   window.location.hash = hash;
+ 	});		
+}
+
+jQuery(document).ready(function($) {
+
+	/* Smooth scroll for nav elements */
+	$(".navbar-collapse ul li a").on('click', function(e) {
+		smoothScroll(e, this.hash);
+	});
+
+	/* Smooth scroll for nav-brand element */
+	$("a.nav-brand").on('click', function(e) {
+		smoothScroll(e, this.hash);
+	});
+
+});

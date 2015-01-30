9c56c8a9ab805b87475de1aeacd8fd6bf72f5de8
Devendra <devendra_rath@yahoo.com>
2015-01-30 20:02:04 +0530

    about us page

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,18 +1,3 @@
-function smoothScroll(e, hash){
- // prevent default anchor click behavior
- e.preventDefault();
-
- // animate
- $('html, body').animate({
-   scrollTop: $(hash).offset().top - 50
- 	}, 300, function(){
-
-   // when done, add hash to url
-   // (default click behaviour)
-   window.location.hash = hash;
- 	});		
-}
-
 jQuery(document).ready(function($) {
 
 	/* Smooth scroll for nav elements */
